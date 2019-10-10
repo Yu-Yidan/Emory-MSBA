91410ff40c20a0d303a3e2756f891a4c4bc47f9f
Yidan Yu <40379284+Yu-Yidan@users.noreply.github.com>
2019-10-10 01:25:05 -0400

    create navigation.js
    create navigation.js

---
+++ js/navigation.js
@@ -0,0 +1,23 @@
+/* https://css-tricks.com/sticky-smooth-active-nav/ ------------------------------------------- */
+let mainNavLinks = document.querySelectorAll("nav ul li a");
+let mainSections = document.querySelectorAll("article section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
